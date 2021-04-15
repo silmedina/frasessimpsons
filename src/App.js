8d10a4c678162b64fb323d6723afeac0ad6d2c6f
sil Medina <medina.silviapatricia@gmail.com>
2021-04-15 01:08:15 -0300

    se agrega spinner

--- src/App.js
+++ src/App.js
@@ -2,15 +2,43 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Button from 'react-bootstrap/Button';
 import Frase from './components/Frase';
+import {useState , useEffect} from 'react' ;
+import Spinner from './components/Spinner';
+
 
 function App() {
+  //crear el state
+  const [personaje, setPersonaje] = useState({});
+  const [cargando, setCargando] = useState(false);
+
+
+  useEffect(()=>{
+    // aqui traigo los datos de la api
+    consultarAPI();
+  }, []);
+
+   const consultarAPI = async () => {
+    //mostrar spinner
+    setCargando(true);
+    const respuesta =  await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
+    const resultado = await respuesta.json();
+    console.log(resultado[0]);
+    //mostrar el componente frase
+    setTimeout(()=>{
+      setCargando(false);
+      setPersonaje(resultado[0]);
+    }, 3000);
+  };
+// (condicion logica)? (codigo cuando es verdadero):(codigo cuando la condicion es falsa)
+  const mostrarComponente = (cargando === true)?(<Spinner></Spinner>):(<Frase personaje={personaje}></Frase>)
+
   return (
     <section className='container d-flex flex-column my-5 align-items-center'>
       <img src={process.env.PUBLIC_URL+'logo.png'} alt="logo de los simpsons" className='w-75'/>
-      <Button variant="warning" className='my-4 w-75'>Obtener frase</Button>
-      <Frase></Frase>
+      <Button variant="warning" className='my-4 w-75' onClick={()=> consultarAPI()}>Obtener frase</Button>
+    {mostrarComponente};
     </section>
   );
-}
+} 
 
 export default App;
--- src/components/Frase.js
+++ src/components/Frase.js
@@ -1,17 +1,17 @@
 import React  from 'react';
 import Card from 'react-bootstrap/Card'
 
-const Frase = () =>{
+const Frase = (props) =>{
    return (
     <Card className='w-100' >
     <Card.Body>
       <div className="row">
         <div className="col-sm-12 col-md 4">
-          <img src="" alt=""/>
+          <img src={props.personaje.image} alt={props.personaje.character}/>
         </div>
         <div className="col-sm 12 col-md 8">
-        <Card.Title>Personaje</Card.Title>
-        <Card.Text>Frase aleatoria dek personaje</Card.Text>
+        <Card.Title>{props.personaje.character}</Card.Title>
+        <Card.Text>{props.personaje.quote}</Card.Text>
         </div>
       </div>
     </Card.Body>
